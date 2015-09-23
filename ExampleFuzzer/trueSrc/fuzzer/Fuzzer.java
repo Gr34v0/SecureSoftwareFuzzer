e59c97939c87c9a64bbd2b6e0e37bbc6494155fe
Gr34v0 <cxg4749@g.rit.edu>
2015-09-23 13:24:15 -0400

    Added support to find all links leading to prodid links in pages.

---
+++ ExampleFuzzer/trueSrc/fuzzer/Fuzzer.java
@@ -0,0 +1,78 @@
+package fuzzer;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+
+public class Fuzzer {
+	
+	static ArrayList<String> commands;
+
+	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		WebClient webClient = new WebClient();
+		webClient.setJavaScriptEnabled(true);
+		
+		commands = new ArrayList<String>();
+		
+		String helpString = "Help - gives information about actions for Fuzzer";
+		commands.add(helpString);
+		
+		//for(String arg:args){}
+		
+		discoverLinks(webClient);
+		guessLinks(webClient);
+		
+		webClient.closeAllWindows();
+
+	}
+	
+	public static List<HtmlAnchor> discoverLinks(WebClient webClient) throws IOException, MalformedURLException{
+		
+		HtmlPage defaultpage = webClient.getPage("http://localhost:8080/bodgeit");
+		
+		List<HtmlAnchor> links = defaultpage.getAnchors();
+		for(HtmlAnchor link : links){
+			System.out.println("Link discovered: " + link.asText() + " @URL=" + link.getHrefAttribute());
+		}
+		
+		System.out.println("--------------------" + "Default Page" + "-------------------------");
+		
+		for(HtmlAnchor link : links){
+			
+			if(link.getHrefAttribute().contains("typeid"))
+			
+			if(link.getHrefAttribute().contains("typeid")){
+				HtmlPage subPage = webClient.getPage("http://localhost:8080/bodgeit/" + link.getHrefAttribute());
+				List<HtmlAnchor> moreLinks = subPage.getAnchors();
+				for(HtmlAnchor linkInSubpage : moreLinks){
+					if(linkInSubpage.getHrefAttribute().contains("prodid")){
+						System.out.println("Link discovered " + linkInSubpage.asText() +" @URL=" + linkInSubpage.getHrefAttribute());
+					}
+				}
+				System.out.println("--------------------" + link.asText() + "-------------------------");
+			}
+		}
+		
+		return links;
+	}
+	
+	public static void guessLinks(WebClient webClient) throws IOException, MalformedURLException{
+		//TODO
+	}
+	
+	public static void auth(){
+		//TODO
+	}
+	
+	public static void help(){
+		System.out.println("Commands: ");
+		for(String command:commands){
+				System.out.println(command);	 }
+				
+	}
+
+}
